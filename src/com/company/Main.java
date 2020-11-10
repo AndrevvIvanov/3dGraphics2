00f45b701971d94d1a9bd77d400a9a9a78a5b072
AndrevvIvanov <andrewivanovviktorovich@gmail.com>
2020-11-10 09:26:27 +0300

    Финальная версия, 1 четверть

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.image.BufferStrategy;
@@ -16,7 +17,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Main extends JFrame {
+public class Main extends JFrame implements KeyListener {
 
     static final int w = 1366;
     static final int h = 768;
@@ -25,16 +26,22 @@ public class Main extends JFrame {
     static ArrayList<V> texture_coordinates = new ArrayList<>();
     static ArrayList<V> normals = new ArrayList<>();
     static ArrayList<V[][]> figures = new ArrayList<>();
-    static double alpha = -90 * (Math.PI / 180);
-    static double beta = 90 * (Math.PI / 180);
-    static double gamma = 180 * (Math.PI / 180);
+    static double d_x = 0;
+    static double d_y = 0;
+    static double alpha = 0;
+    static double beta = 0;
+    static double gamma = 0;
+    static double c_x = 1;
+    static double c_y = 1;
+    static double c_z = 1;
+
     static BufferedImage texture = null;
     static double[][] z_buffer = new double[Main.w][Main.h];
 
 
     public static void draw(Graphics2D g) {
         BufferedImage img = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
-        Render.render(img, figures, texture, alpha, beta, gamma, z_buffer);
+        Render.render(img, figures, texture, d_x, d_y, 0, alpha, beta, gamma, c_x, c_y, c_z, z_buffer);
         g.drawImage(img, 0, 0, null);
     }
 
@@ -105,7 +112,7 @@ public class Main extends JFrame {
         jf.setVisible(true);
         jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jf.createBufferStrategy(2);
-
+        jf.addKeyListener(jf);
 
         while (true) {
             long frameLength = 1000 / 60; //пытаемся работать из рассчета  60 кадров в секунду
@@ -113,10 +120,6 @@ public class Main extends JFrame {
             BufferStrategy bs = jf.getBufferStrategy();
             Graphics2D g = (Graphics2D) bs.getDrawGraphics();
             g.clearRect(0, 0, jf.getWidth(), jf.getHeight());
-
-            alpha += 15 * (Math.PI / 180);
-            //beta += 15 * (Math.PI / 180);
-            //gamma += 15 * (Math.PI / 180);
 
             draw(g);
 
@@ -134,8 +137,61 @@ public class Main extends JFrame {
                     z_buffer[i][j] = Integer.MAX_VALUE;
                 }
             }
-
         }
+    }
+
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_W) {
+            alpha = alpha - 15 * (Math.PI / 180);
+        }
+        if (e.getKeyCode() == KeyEvent.VK_S) {
+            alpha = alpha + 15 * (Math.PI / 180);
+        }
+        if (e.getKeyCode() == KeyEvent.VK_A) {
+            beta = beta - 15 * (Math.PI / 180);
+        }
+        if (e.getKeyCode() == KeyEvent.VK_D) {
+            beta = beta + 15 * (Math.PI / 180);
+        }
+        if (e.getKeyCode() == KeyEvent.VK_Q) {
+            gamma = gamma - 15 * (Math.PI / 180);
+        }
+        if (e.getKeyCode() == KeyEvent.VK_E) {
+            gamma = gamma + 15 * (Math.PI / 180);
+        }
+
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            d_x = d_x + 10;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+            d_x = d_x - 10;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_UP) {
+            d_y = d_y - 10;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+            d_y = d_y + 10;
+        }
+
+        if (e.getKeyCode() == KeyEvent.VK_R) {
+            c_x = c_x * 0.9;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_T) {
+            c_x = c_x / 0.9;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_F) {
+            c_y = c_y * 0.9;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_G) {
+            c_y = c_y / 0.9;
+        }
+        /*if (e.getKeyCode() == KeyEvent.VK_N) {
+            c_z = c_z * 0.9;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_M) {
+            c_z = c_z / 0.9;
+        }*/
+
 
     }
 
@@ -144,6 +200,5 @@ public class Main extends JFrame {
 
     //Вызывается когда клавиша отпущена пользователем, обработка события аналогична keyPressed
     public void keyReleased(KeyEvent e) {
-
     }
 }
--- src/com/company/Render.java
+++ src/com/company/Render.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 public class Render {
 
 
-    public static void render(BufferedImage img, ArrayList<V[][]> figures, BufferedImage texture, double alpha, double beta, double gamma, double[][] z_buffer) {
+    public static void render(BufferedImage img, ArrayList<V[][]> figures, BufferedImage texture, double d_x, double d_y, double d_z, double alpha, double beta, double gamma, double c_x, double c_y, double c_z, double[][] z_buffer) {
 
         for (int i = 0; i < figures.size(); i++) {
             V v1 = figures.get(i)[0][0];
@@ -20,12 +20,37 @@ public class Render {
             V normal2 = figures.get(i)[1][2];
             V normal3 = figures.get(i)[2][2];
 
-            v1 = rotate(alpha, beta, gamma, v1);
-            v2 = rotate(alpha, beta, gamma, v2);
-            v3 = rotate(alpha, beta, gamma, v3);
-            normal1 = rotate(alpha, beta, gamma, normal1);
-            normal2 = rotate(alpha, beta, gamma, normal2);
-            normal3 = rotate(alpha, beta, gamma, normal3);
+            V v1_help = new V(new double[]{v1.arr[0], v1.arr[1], v1.arr[2], 1});
+            V v2_help = new V(new double[]{v2.arr[0], v2.arr[1], v2.arr[2], 1});
+            V v3_help = new V(new double[]{v3.arr[0], v3.arr[1], v3.arr[2], 1});
+            V normal1_help = new V(new double[]{normal1.arr[0], normal1.arr[1], normal1.arr[2], 1});
+            V normal2_help = new V(new double[]{normal2.arr[0], normal2.arr[1], normal2.arr[2], 1});
+            V normal3_help = new V(new double[]{normal3.arr[0], normal3.arr[1], normal3.arr[2], 1});
+            double w = 0.1;
+            double h = 0.1;
+            double d = 0.1;
+            v1_help = Projection(v1_help.arr[0], v1_help.arr[1], w, h, d).vectorMult(Translocation(d_x, d_y, d_z).vectorMult(Rotation(alpha, beta, gamma).vectorMult(Scale(c_x, c_y, c_z).vectorMult(v1_help))));
+            v2_help = Projection(v2_help.arr[0], v2_help.arr[1], w, h, d).vectorMult(Translocation(d_x, d_y, d_z).vectorMult(Rotation(alpha, beta, gamma).vectorMult(Scale(c_x, c_y, c_z).vectorMult(v2_help))));
+            v3_help = Projection(v3_help.arr[0], v3_help.arr[1], w, h, d).vectorMult(Translocation(d_x, d_y, d_z).vectorMult(Rotation(alpha, beta, gamma).vectorMult(Scale(c_x, c_y, c_z).vectorMult(v3_help))));
+            //normal1_help = Projection(normal1_help.arr[0], normal1_help.arr[1], w, h, d).vectorMult(Translocation(d_x, d_y, d_z).vectorMult(Rotation(alpha, beta, gamma).vectorMult(normal1_help)));
+            //normal2_help = Projection(normal2_help.arr[0], normal2_help.arr[1], w, h, d).vectorMult(Translocation(d_x, d_y, d_z).vectorMult(Rotation(alpha, beta, gamma).vectorMult(normal2_help)));
+            //normal3_help = Projection(normal3_help.arr[0], normal3_help.arr[1], w, h, d).vectorMult(Translocation(d_x, d_y, d_z).vectorMult(Rotation(alpha, beta, gamma).vectorMult(normal3_help)));
+
+           /* v1_help = Translocation(d_x, d_y, d_z).vectorMult(Rotation(alpha, beta, gamma).vectorMult(Scale(c_x, c_y, c_z).vectorMult(v1_help)));
+            v2_help = Translocation(d_x, d_y, d_z).vectorMult(Rotation(alpha, beta, gamma).vectorMult(Scale(c_x, c_y, c_z).vectorMult(v2_help)));
+            v3_help = Translocation(d_x, d_y, d_z).vectorMult(Rotation(alpha, beta, gamma).vectorMult(Scale(c_x, c_y, c_z).vectorMult(v3_help)));*/
+            normal1_help = Translocation(d_x, d_y, d_z).vectorMult(Rotation(alpha, beta, gamma).vectorMult(normal1_help));
+            normal2_help = Translocation(d_x, d_y, d_z).vectorMult(Rotation(alpha, beta, gamma).vectorMult(normal2_help));
+            normal3_help = Translocation(d_x, d_y, d_z).vectorMult(Rotation(alpha, beta, gamma).vectorMult(normal3_help));
+
+
+            v1 = new V(new double[]{v1_help.arr[0] / v1_help.arr[3], v1_help.arr[1] / v1_help.arr[3], v1_help.arr[2] / v1_help.arr[3]});
+            v2 = new V(new double[]{v2_help.arr[0] / v2_help.arr[3], v2_help.arr[1] / v2_help.arr[3], v2_help.arr[2] / v2_help.arr[3]});
+            v3 = new V(new double[]{v3_help.arr[0] / v3_help.arr[3], v3_help.arr[1] / v3_help.arr[3], v3_help.arr[2] / v3_help.arr[3]});
+            normal1 = new V(new double[]{normal1_help.arr[0] / normal1_help.arr[3], normal1_help.arr[1] / normal1_help.arr[3], normal1_help.arr[2] / normal1_help.arr[3]});
+            normal2 = new V(new double[]{normal2_help.arr[0] / normal2_help.arr[3], normal2_help.arr[1] / normal2_help.arr[3], normal2_help.arr[2] / normal2_help.arr[3]});
+            normal3 = new V(new double[]{normal3_help.arr[0] / normal3_help.arr[3], normal3_help.arr[1] / normal3_help.arr[3], normal3_help.arr[2] / normal3_help.arr[3]});
+
 
             V AB = v2.sum(v1.scalarMult(-1));
             V AC = v3.sum(v1.scalarMult(-1));
@@ -39,33 +64,53 @@ public class Render {
             double n3 = normal3.scalarProduct(l);
 
             if (check >= 0) {
-                V v = new V(new double[]{500, 500, 0});
-                v1 = v1.sum(v);
-                v2 = v2.sum(v);
-                v3 = v3.sum(v);
                 Triangle(img, z_buffer, v1, v2, v3, n1, n2, n3, texture_coordinates1, texture_coordinates2, texture_coordinates3, texture, AB, AC);
             }
         }
     }
 
 
-    public static V rotate(double alpha, double beta, double gamma, V v) {
-        Matrix Rx = new Matrix(new double[][]
-                {{1, 0, 0},
-                        {0, Math.cos(alpha), -Math.sin(alpha)},
-                        {0, Math.sin(alpha), Math.cos(alpha)}});
-        Matrix Ry = new Matrix(new double[][]
-                {{Math.cos(beta), 0, Math.sin(beta)},
-                        {0, 1, 0},
-                        {-Math.sin(beta), 0, Math.cos(beta)}});
-        Matrix Rz = new Matrix(new double[][]
-                {{Math.cos(gamma), -Math.sin(gamma), 0},
-                        {Math.sin(gamma), Math.cos(gamma), 0},
-                        {0, 0, 1}});
-        Matrix R = Rx.Mult((Ry.Mult(Rz)));
-        return R.vectorMult(v);
+    public static Matrix Projection(double x, double y, double w, double h, double d) {
+        return new Matrix(new double[][]{{w / 2, 0, 0, x + w / 2},
+                {0, h / 2, 0, y + h / 2},
+                {0, 0, d / 2, d / 2},
+                {0, 0, 0, 1}});
     }
 
+
+    public static Matrix Translocation(double d_x, double d_y, double d_z) {
+        return new Matrix(new double[][]{{1, 0, 0, d_x},
+                {0, 1, 0, d_y},
+                {0, 0, 1, d_z},
+                {0, 0, 0, 1}});
+    }
+
+
+    public static Matrix Rotation(double alpha, double beta, double gamma) {
+        Matrix Rx = new Matrix(new double[][]
+                {{1, 0, 0, 0},
+                        {0, Math.cos(alpha), -Math.sin(alpha), 0},
+                        {0, Math.sin(alpha), Math.cos(alpha), 0},
+                        {0, 0, 0, 1}});
+        Matrix Ry = new Matrix(new double[][]
+                {{Math.cos(beta), 0, Math.sin(beta), 0},
+                        {0, 1, 0, 0},
+                        {-Math.sin(beta), 0, Math.cos(beta), 0},
+                        {0, 0, 0, 1}});
+        Matrix Rz = new Matrix(new double[][]
+                {{Math.cos(gamma), -Math.sin(gamma), 0, 0},
+                        {Math.sin(gamma), Math.cos(gamma), 0, 0},
+                        {0, 0, 1, 0},
+                        {0, 0, 0, 1}});
+        return Rx.Mult((Ry.Mult(Rz)));
+    }
+
+    public static Matrix Scale(double c_x, double c_y, double c_z) {
+        return new Matrix(new double[][]{{c_x, 0, 0, 0},
+                {0, c_y, 0, 0},
+                {0, 0, c_z, 0},
+                {0, 0, 0, 1}});
+    }
 
     public static void renderLine(BufferedImage img, int x1, int y1, int x2, int y2, Color c) {
         if (x1 == x2 && y1 == y2) {
